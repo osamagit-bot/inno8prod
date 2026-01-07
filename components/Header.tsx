395cfaed9c17db7b7f874d5626c2c006abb558a0
John Doe <johndoe@gmail.com>
2026-01-07 11:41:18 +0430

    Some Responsive Issues Solved

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { useColors } from '../contexts/ColorContext'
-import { API_ENDPOINTS, getImageUrl, API_CONFIG } from '../lib/api'
+import { API_ENDPOINTS, getImageUrl } from '../lib/api'
 import { fallbackData } from '../lib/fallbackData'
 
 interface SiteSettings {
@@ -143,7 +142,7 @@ export default function Header() {
             </div>
           </div>
           
-          <div className="flex items-center space-x-6 text-white">
+          <div className="flex items-center space-x-16 text-white">
             <div className="flex items-center space-x-2">
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
@@ -183,7 +182,7 @@ export default function Header() {
         {/* Split Background */}
         <div className="absolute inset-0 flex">
           <div className="w-3/5" style={{ backgroundColor: '#fff' }}></div>
-          <div className="w-2/5" style={{ backgroundColor: colors.primary_color }}></div>
+          <div className="absolute right-0 top-0 bottom-0 w-[30rem]" style={{ backgroundColor: colors.primary_color }}></div>
         </div>
       </div>
 
